5a2f1b8b28ee511c7b884d8f98ac7b392be00f71
Papa Abdallah Pathé Cissé <yourpathe196@gmail.com>
2026-04-06 17:47:09 +0100

    Use @for to list objects in component

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,25 +1,22 @@
-import { Component, input } from '@angular/core';
-import { HousingLocationInfo } from '../housinglocation';
+import {Component, input} from '@angular/core';
+import {HousingLocationInfo} from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
   template: `
     <section class="listing">
       <img
-      class="listin-photo"
-       [src]="housingLocation().photo"
-       alt="Exterior photo of {{ housingLocation().name }}"
-       crossorigin
-       >
-       <h2 class="listing-heading">{{housingLocation().name}}</h2>
-       <p class="listing-location">{{housingLocation().city}}</p>
+        class="listing-photo"
+        [src]="housingLocation().photo"
+        alt="Exterior photo of {{ housingLocation().name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation().name }}</h2>
+      <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>
     </section>
   `,
-  styles: ``,
+  styleUrls: ['./housing-location.css'],
 })
 export class HousingLocation {
-
   housingLocation = input.required<HousingLocationInfo>();
-
 }
